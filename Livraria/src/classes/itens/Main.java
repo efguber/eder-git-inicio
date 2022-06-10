844ce5f2bb9e585b539117aea60570b2c37cae19
Eder Guber <104037845+efguber@users.noreply.github.com>
2022-06-09 21:55:54 -0300

    Estante

--- Livraria/src/classes/itens/Main.java
+++ Livraria/src/classes/itens/Main.java
@@ -1,14 +1,31 @@
 package classes.itens;
 
+import classes.guardados.Estante;
+
+
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Livro l = new Livro();
-        l.setTitulo("Harry Potter e a Autora Transfóbica");
-        l.avaliar();
-        l.avaliar();
-        l.avaliar();
-        System.out.println(l.getTotalRating());
+        Estante e = new Estante(5);
+        Item item = null;
+        System.out.print("Escolha uma opcao de local na estante:");
+        System.out.print("1");
+        System.out.print("2");
+        System.out.print("3");
+        System.out.print("4");
+        System.out.print("5");
+        String escolha = in.nextLine();
+        for (int i = 0; i < 5; i++) {
+
+            }
+
+        }
+
     }
 
-}
+
+
+
